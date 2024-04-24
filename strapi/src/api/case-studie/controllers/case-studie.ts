a4ab4b5d4fa3e7c5b4acfa71d383cad61ae4522d
Михаил <115717017+mykhail-druz@users.noreply.github.com>
2024-04-24 19:51:31 +0300

    deleted strapi

--- strapi/src/api/case-studie/controllers/case-studie.ts
+++
@@ -1,9 +0,0 @@
-/**
- * case-studie controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController(
-  'api::case-studie.case-studie'
-)
--- strapi/src/api/case-studie/routes/case-studie.ts
+++
@@ -1,9 +0,0 @@
-/**
- * case-studie router
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreRouter(
-  'api::case-studie.case-studie'
-)
